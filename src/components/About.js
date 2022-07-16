1847c3453139b1656abd8c73b6fe63ce9ab55172
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-07-16 12:23:22 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+
 import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am a software developer</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"></img>
+    </div>
+    );
 }
 
 export default About;
